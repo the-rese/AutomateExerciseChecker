937e2ecd8fcf7e9bbf16812d8c0b86dfd3cb2262
Therese Bolabola <terebol0314@gmail.com>
2022-04-06 11:03:04 +0800

    imported the module class

--- pose detection renote/pushup.py
+++ pose detection renote/pushup.py
@@ -1,26 +1,10 @@
+from typing import Counter
+import posedetectionmodule as pd
 import cv2
 import mediapipe as mp
-import numpy as np
 from tkinter import *
 from tkinter import messagebox
-mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
-
-
-def calculate_angle(a, b, c):
-    a = np.array(a)  # First
-    b = np.array(b)  # Mid
-    c = np.array(c)  # End
-
-    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - \
-        np.arctan2(a[1]-b[1], a[0]-b[0])
-    angle = np.abs(radians*180.0/np.pi)
-
-    if angle > 180.0:
-        angle = 360-angle
-
-    return angle
-
 
 # VIDEO FEED
 cap = cv2.VideoCapture('LowResExercises/wide-pushups.mp4')
@@ -30,126 +14,81 @@ counter = 0
 stage = None
 
 # Setup mediapipe instance
-with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-    while cap.isOpened():
-        ret, frame = cap.read()
+pose = pd.PoseDetection()
 
-        # Recolor image to RGB
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
+while cap.isOpened():
+    ret, frame = cap.read()
 
-        # Make detection
-        results = pose.process(image)
+    image = pose.makeDetection(frame)
 
-        # Recolor back to BGR
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    # Extract landmarks
+    try:
+        landmarks = pose.extractLandmarks(image)
 
-        # Extract landmarks
-        try:
-            landmarks = results.pose_landmarks.landmark
+        # Get coordinates
+        # A good pushup should aim to bend the elbows at or below a 90 degree angle. Since the basis is the elbow angle, both the wide and close grip pushup variations can be accounted for
 
-            # Get coordinates
-            # A good pushup should aim to bend the elbows at or below a 90 degree angle. Since the basis is the elbow angle, both the wide and close grip pushup variations can be accounted for
+        # hand coordinates
+        left_hand = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                     landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+        right_hand = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
+                      landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
 
-            # hand coordinates
-            left_hand = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
-                         landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
-            right_hand = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
-                          landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+        # elbow coordinates
+        left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+                      landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+        right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
+                       landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
 
-            # elbow coordinates
-            left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
-                          landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-            right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
-                           landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+        # shoulder coordinates
+        left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                         landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+        right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
+                          landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
 
-            # shoulder coordinates
-            left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
-                             landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-            right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
-                              landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+        # hip coordinates
+        left_hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
+                    landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+        right_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
+                     landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+        # Calculate angle
+        elbow_angle = pose.calculateAngle(left_hand, left_elbow, left_shoulder)
+        # Check Pushup Variation
+        arm_angle = pose.calculateAngle(left_elbow, left_shoulder, left_hip)
 
-            # hip coordinates
-            left_hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
-                        landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
-            right_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
-                         landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
-            # Calculate angle
-            elbow_angle = calculate_angle(left_hand, left_elbow, left_shoulder)
-            # Check Pushup Variation
-            arm_angle = calculate_angle(left_elbow, left_shoulder, left_hip)
+        pose.visualizeAngle(image, elbow_angle, left_elbow)
 
-            # Visualize elbow angle
-            cv2.putText(image, str("{:.2f}".format(elbow_angle)),
-                        tuple(np.multiply(right_elbow,
-                              [640, 480]).astype(int)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,
-                                                        255, 255), 2, cv2.LINE_AA
-                        )
+        # Visualize arm angle
+        # cv2.putText(image, str("{:.2f}".format(arm_angle)),
+        #             tuple(np.multiply(left_shoulder,
+        #                   [640, 480]).astype(int)),
+        #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,
+        #                                             255, 255), 2, cv2.LINE_AA
+        #             )
 
-            # Visualize arm angle
-            # cv2.putText(image, str("{:.2f}".format(arm_angle)),
-            #             tuple(np.multiply(left_shoulder,
-            #                   [640, 480]).astype(int)),
-            #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,
-            #                                             255, 255), 2, cv2.LINE_AA
-            #             )
+        # pushup counter logic
+        if elbow_angle < 90:
+            stage = "down"
+        if elbow_angle > 150 and stage == 'down':
+            stage = "up"
+            counter += 1
+            print(counter)
 
-            # pushup counter logic
-            if elbow_angle < 90:
-                stage = "down"
-            if elbow_angle > 150 and stage == 'down':
-                stage = "up"
-                counter += 1
-                print(counter)
+    except:
+        pass
 
-        except:
-            pass
+    pose.visualizeRepCounter(image, "PUSHUPS", counter, stage)
 
-        # Render curl counter
-        # Setup status box
-        cv2.rectangle(image, (0, 0), (320, 50), (245, 117, 16), -1)
+    cv2.imshow('Mediapipe Feed', image)
 
-        # Exercise performed
-        cv2.putText(image, 'EXERCISE', (15, 12),
-                    cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.putText(image, 'PUSHUPS',
-                    (10, 40),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2, cv2.LINE_AA)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        # if 90 < pushup_angle < 160 remark "Pushups not deep enough. Low muscle activation"
+        # if pushup_angle < 50, remark "Pushups too deep. Risk shoulder injury in the long term"
+        # if pushup_angle 80 to 100, remark "Satisfactory Form"
+        messagebox.showinfo("Exercise Summary",
+                            "Exercise:Pushups\nReps:10\nRemarks:Satisfactory Form")
 
-        # Rep data
-        cv2.putText(image, 'REPS', (180, 12),
-                    cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.putText(image, str(counter),
-                    (180, 40),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2, cv2.LINE_AA)
+        break
 
-        # Stage data
-        cv2.putText(image, 'STAGE', (230, 12),
-                    cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.putText(image, stage,
-                    (230, 40),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2, cv2.LINE_AA)
-
-        # Render detections
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                  mp_drawing.DrawingSpec(
-                                      color=(245, 117, 66), thickness=2, circle_radius=2),
-                                  mp_drawing.DrawingSpec(
-                                      color=(245, 66, 230), thickness=2, circle_radius=2)
-                                  )
-
-        cv2.imshow('Mediapipe Feed', image)
-
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            # if 90 < pushup_angle < 160 remark "Pushups not deep enough. Low muscle activation"
-            # if pushup_angle < 50, remark "Pushups too deep. Risk shoulder injury in the long term"
-            # if pushup_angle 80 to 100, remark "Satisfactory Form"
-            messagebox.showinfo("Exercise Summary",
-                                "Exercise:Pushups\nReps:10\nRemarks:Satisfactory Form")
-
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
--- pose detection renote/situp.py
+++ pose detection renote/situp.py
@@ -1,24 +1,8 @@
+import posedetectionmodule as pd
 import cv2
 import mediapipe as mp
-import numpy as np
 from tkinter import messagebox
-mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
-
-
-def calculate_angle(a, b, c):
-    a = np.array(a)  # First
-    b = np.array(b)  # Mid
-    c = np.array(c)  # End
-
-    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - \
-        np.arctan2(a[1]-b[1], a[0]-b[0])
-    angle = np.abs(radians*180.0/np.pi)
-
-    if angle > 180.0:
-        angle = 360-angle
-
-    return angle
 
 
 def reduceResolution():
@@ -36,95 +20,48 @@ cap = cv2.VideoCapture('LowResExercises/situps.mp4')
 counter = 0
 stage = None
 
-# Setup mediapipe instance
-with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-    while cap.isOpened():
-        ret, frame = cap.read()
+pose = pd.PoseDetection()
 
-        # Recolor image to RGB
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
+while cap.isOpened():
+    ret, frame = cap.read()
 
-        # Make detection
-        results = pose.process(image)
+    image = pose.makeDetection(frame)
 
-        # Recolor back to BGR
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    # Extract landmarks
+    try:
+        landmarks = pose.extractLandmarks(image)
 
-        # Extract landmarks
-        try:
-            landmarks = results.pose_landmarks.landmark
+        # Get coordinates
+        # A good sit-up
+        shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
+                    landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+        hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
+               landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+        knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,
+                landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
 
-            # Get coordinates
-            # A good sit-up
-            shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
-                        landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-            hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
-                   landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
-            knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,
-                    landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+        # Calculate angle
+        angle = pose.calculateAngle(shoulder, hip, knee)
+        pose.visualizeAngle(image, angle, hip)
 
-            # Calculate angle
-            angle = calculate_angle(shoulder, hip, knee)
+        # Sit-up counter logic
+        if angle < 40:
+            stage = "up"
+        if angle > 100 and stage == 'up':
+            stage = "down"
+            counter += 1
+            print(counter)
 
-            # Visualize angle
-            cv2.putText(image, str(angle),
-                        tuple(np.multiply(hip, [640, 480]).astype(int)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,
-                                                        255, 255), 2, cv2.LINE_AA
-                        )
+    except:
+        pass
 
-            # Sit-up counter logic
-            if angle < 40:
-                stage = "up"
-            if angle > 100 and stage == 'up':
-                stage = "down"
-                counter += 1
-                print(counter)
+    pose.visualizeRepCounter(image, "SIT-UPS", counter, stage)
+    cv2.imshow('Mediapipe Feed', image)
 
-        except:
-            pass
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        messagebox.showinfo("Exercise Summary",
+                            "Exercise:Sit Ups\nReps:3\nRemarks: Squats not deep enough.")
+        break
 
-        # Render curl counter
-        # Setup status box
-        cv2.rectangle(image, (0, 0), (320, 50), (245, 117, 16), -1)
-
-        # Exercise performed
-        cv2.putText(image, 'EXERCISE', (15, 12),
-                    cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.putText(image, 'SITUPS',
-                    (10, 40),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2, cv2.LINE_AA)
-
-        # Rep data
-        cv2.putText(image, 'REPS', (180, 12),
-                    cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.putText(image, str(counter),
-                    (180, 40),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2, cv2.LINE_AA)
-
-        # Stage data
-        cv2.putText(image, 'STAGE', (230, 12),
-                    cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.putText(image, stage,
-                    (230, 40),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2, cv2.LINE_AA)
-
-        # Render detections
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                  mp_drawing.DrawingSpec(
-                                      color=(245, 117, 66), thickness=2, circle_radius=2),
-                                  mp_drawing.DrawingSpec(
-                                      color=(255, 255, 255), thickness=2, circle_radius=2)
-                                  )
-
-        cv2.imshow('Mediapipe Feed', image)
-
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            messagebox.showinfo("Exercise Summary",
-                                "Exercise:Sit Ups\nReps:3\nRemarks: Squats not deep enough.")
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
